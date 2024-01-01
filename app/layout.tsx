7ecf861f59da7d9fe44bad70f4d95af252c31fed
iavianm <iavianm@mail.ru>
2024-01-02 01:00:48 +0200

    init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 const roboto = Roboto({
   subsets: ["cyrillic"],
