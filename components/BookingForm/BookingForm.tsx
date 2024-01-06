c06b75ba231d1428b15e4e7aacb19d7a78fd6628
iavianm <iavianm@mail.ru>
2024-01-06 22:30:24 +0200

    update booking form

--- components/BookingForm/BookingForm.tsx
+++ components/BookingForm/BookingForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import styles from "./BookingForm.module.css";
-import React from "react";
-import { Formik, ErrorMessage, Field } from "formik";
+import React, { useEffect, useState } from "react";
+import { Formik, ErrorMessage, Field, useFormik } from "formik";
 import {
   StyledForm,
   FormRow,
@@ -15,8 +15,9 @@ import {
   FormRowName,
 } from "./StyledComponents";
 import NumberInput from "./NumberInput";
-import { createBooking } from "@/api/api";
+import { createBooking, getLatestBookings } from "@/api/api";
 import { BookingSchema, today } from "@/components/BookingForm/BookingSchema";
+import { options } from "../../content/additionalOptionsContent.json";
 
 interface FormValues {
   arrivalDate: string;
@@ -46,8 +47,57 @@ const initialValues: FormValues = {
   email: "",
 };
 
+interface TotalCottages {
+  [key: string]: number;
+}
+
+interface Booking {
+  cottage: string;
+  arrivalDate: string;
+  departureDate: string;
+  name: string;
+  adults: number;
+  children: number;
+  phone: string;
+  email: string;
+  additional: Array<string>;
+}
+
+const checkAvailability = (
+  cottage: string,
+  arrival: string,
+  departure: string,
+  bookings: Booking[],
+  totalCottages: TotalCottages,
+) => {
+  const arrivalDate = new Date(arrival);
+  const departureDate = new Date(departure);
+
+  const countBookings = bookings.filter((booking) => {
+    if (booking.cottage !== cottage) {
+      return false;
+    }
+
+    const bookedArrival = new Date(booking.arrivalDate);
+    const bookedDeparture = new Date(booking.departureDate);
+
+    return !(bookedDeparture <= arrivalDate || bookedArrival >= departureDate);
+  }).length;
+
+  return countBookings < totalCottages[cottage];
+};
+
 const BookingForm: React.FC<Props> = ({ onSuccess }) => {
   const capitalize = require("capitalize");
+  const [bookedDates, setBookedDates] = useState([]);
+
+  useEffect(() => {
+    getLatestBookings().then((res) => {
+      setBookedDates(res);
+    });
+  }, []);
+
+  const [availableCottages, setAvailableCottages] = useState<string[]>([]);
 
   return (
     <Formik
@@ -57,145 +107,171 @@ const BookingForm: React.FC<Props> = ({ onSuccess }) => {
         console.log(values);
         createBooking(values)
           .then((r) => {
-            console.log(r);
             actions.setSubmitting(false);
             onSuccess();
           })
-          .catch((e) => console.log(e));
+          .catch((e) => {
+            console.log(e);
+            actions.setSubmitting(false);
+          });
       }}
     >
-      {({ values, setFieldValue, isSubmitting, isValid, dirty }) => (
-        <StyledForm>
-          <FormRow>
-            <FieldContainer>
-              <label htmlFor="arrivalDate">Дата заезда *</label>
-              <StyledField
-                type="date"
-                name="arrivalDate"
-                min={today}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
-                  setFieldValue("arrivalDate", e.target.value);
+      {(formikProps) => {
+        const { values, setFieldValue, isSubmitting, isValid, dirty } =
+          formikProps;
 
-                  if (e.target.value >= values.departureDate) {
-                    const newDepartureDate = new Date(e.target.value);
-                    newDepartureDate.setDate(newDepartureDate.getDate() + 1);
-                    setFieldValue(
-                      "departureDate",
-                      newDepartureDate.toISOString().split("T")[0],
-                    );
+        const totalCottages = {
+          riviera: 3,
+          grandis: 2,
+        };
+
+        useEffect(() => {
+          const newAvailableCottages = Object.keys(totalCottages).filter(
+            (cottageType) =>
+              checkAvailability(
+                cottageType,
+                values.arrivalDate,
+                values.departureDate,
+                bookedDates,
+                totalCottages,
+              ),
+          );
+
+          setAvailableCottages(newAvailableCottages);
+        }, [values.arrivalDate, values.departureDate, bookedDates]);
+
+        return (
+          <StyledForm onSubmit={formikProps.handleSubmit}>
+            <FormRow>
+              <FieldContainer>
+                <label htmlFor="arrivalDate">Дата заезда *</label>
+                <StyledField
+                  type="date"
+                  name="arrivalDate"
+                  min={today}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
+                    setFieldValue("arrivalDate", e.target.value);
+
+                    if (e.target.value >= values.departureDate) {
+                      const newDepartureDate = new Date(e.target.value);
+                      newDepartureDate.setDate(newDepartureDate.getDate() + 1);
+                      setFieldValue(
+                        "departureDate",
+                        newDepartureDate.toISOString().split("T")[0],
+                      );
+                    }
+                  }}
+                />
+                <ErrorMessageContainer>
+                  <ErrorMessage name="arrivalDate" component={ErrorText} />
+                </ErrorMessageContainer>
+              </FieldContainer>
+              <FieldContainer>
+                <label htmlFor="departureDate">Дата выезда *</label>
+                <StyledField
+                  type="date"
+                  name="departureDate"
+                  min={
+                    values.arrivalDate
+                      ? new Date(values.arrivalDate).toISOString().split("T")[0]
+                      : today
                   }
-                }}
-              />
-              <ErrorMessageContainer>
-                <ErrorMessage name="arrivalDate" component={ErrorText} />
-              </ErrorMessageContainer>
-            </FieldContainer>
+                />
+                <ErrorMessageContainer>
+                  <ErrorMessage name="departureDate" component={ErrorText} />
+                </ErrorMessageContainer>
+              </FieldContainer>
+            </FormRow>
+
             <FieldContainer>
-              <label htmlFor="departureDate">Дата выезда *</label>
-              <StyledField
-                type="date"
-                name="departureDate"
-                min={
-                  values.arrivalDate
-                    ? new Date(values.arrivalDate).toISOString().split("T")[0]
-                    : today
-                }
-              />
+              <label htmlFor="cottage">Выберите коттедж *</label>
+              <Field as="select" name="cottage" className={styles.selectStyle}>
+                <option value="">Выбрать...</option>
+                {availableCottages.length > 0 ? (
+                  availableCottages.map((cottageType) => (
+                    <option key={cottageType} value={cottageType}>
+                      {capitalize(cottageType)}
+                    </option>
+                  ))
+                ) : (
+                  <option value="">Все виллы заняты в этот период</option>
+                )}
+              </Field>
               <ErrorMessageContainer>
-                <ErrorMessage name="departureDate" component={ErrorText} />
+                <ErrorMessage name="cottage" component={ErrorText} />
               </ErrorMessageContainer>
             </FieldContainer>
-          </FormRow>
 
-          <FieldContainer>
-            <label htmlFor="cottage">Выберите коттедж *</label>
-            <Field as="select" name="cottage" className={styles.selectStyle}>
-              <option value="">Выбрать...</option>
-              <option value="cottage1">Коттедж 1</option>
-              <option value="cottage2">Коттедж 2</option>
-              {/* Добавьте дополнительные опции */}
-            </Field>
-            <ErrorMessageContainer>
-              <ErrorMessage name="cottage" component={ErrorText} />
-            </ErrorMessageContainer>
-          </FieldContainer>
-
-          <FieldContainer>
-            <label htmlFor="additional">Дополнительные опции</label>
-            <Field as="select" name="additional" className={styles.selectStyle}>
-              <option value="">Выбрать...</option>
-              {[
-                "мангал",
-                "квадроцикл",
-                "эндуро",
-                "снегоход",
-                "собака",
-                "самовар",
-                "ранний заезд",
-                "поздний заезд",
-                "ранний выезд",
-                "поздний выезд",
-              ].map((option) => (
-                <option key={option} value={capitalize(option)}>
-                  {capitalize(option)}
-                </option>
-              ))}
-            </Field>
-            <ErrorMessageContainer>
-              <ErrorMessage name="additional" component={ErrorText} />
-            </ErrorMessageContainer>
-          </FieldContainer>
-
-          <NumberContainer>
-            <FormRowNumber>
-              <NumberInput label="Взрослые" name="adults" />
-
-              <NumberInput label="Дети" name="children" />
-            </FormRowNumber>
-          </NumberContainer>
-
-          <FormRow>
             <FieldContainer>
-              <label htmlFor="phone">Телефон *</label>
-              <StyledField
-                type="tel"
-                name="phone"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  const trimmedValue = e.target.value.trim();
-                  setFieldValue("phone", trimmedValue);
-                }}
-                placeholder="+79998887766"
-              />
+              <label htmlFor="additional">Дополнительные опции</label>
+              <Field
+                as="select"
+                name="additional"
+                className={styles.selectStyle}
+              >
+                <option value="">Выбрать...</option>
+                {options.map((option) => (
+                  <option key={option} value={capitalize(option)}>
+                    {capitalize(option)}
+                  </option>
+                ))}
+              </Field>
               <ErrorMessageContainer>
-                <ErrorMessage name="phone" component={ErrorText} />
+                <ErrorMessage name="additional" component={ErrorText} />
               </ErrorMessageContainer>
             </FieldContainer>
-            <FieldContainer>
-              <label htmlFor="email">Email</label>
-              <StyledField
-                type="email"
-                name="email"
-                placeholder="email@gmail.com"
-              />
-              <ErrorMessageContainer>
-                <ErrorMessage name="email" component={ErrorText} />
-              </ErrorMessageContainer>
-            </FieldContainer>
-          </FormRow>
 
-          <FormRowName>
-            <label htmlFor="name">Ваше имя</label>
-            <StyledField type="text" name="name" />
-          </FormRowName>
+            <NumberContainer>
+              <FormRowNumber>
+                <NumberInput label="Взрослые" name="adults" />
 
-          <StyledButton
-            type="submit"
-            disabled={!isValid || !dirty || isSubmitting}
-          >
-            Создать бронирование
-          </StyledButton>
-        </StyledForm>
-      )}
+                <NumberInput label="Дети" name="children" />
+              </FormRowNumber>
+            </NumberContainer>
+
+            <FormRow>
+              <FieldContainer>
+                <label htmlFor="phone">Телефон *</label>
+                <StyledField
+                  type="tel"
+                  name="phone"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    const trimmedValue = e.target.value.trim();
+                    setFieldValue("phone", trimmedValue);
+                  }}
+                  placeholder="+79998887766"
+                />
+                <ErrorMessageContainer>
+                  <ErrorMessage name="phone" component={ErrorText} />
+                </ErrorMessageContainer>
+              </FieldContainer>
+              <FieldContainer>
+                <label htmlFor="email">Email</label>
+                <StyledField
+                  type="email"
+                  name="email"
+                  placeholder="email@gmail.com"
+                />
+                <ErrorMessageContainer>
+                  <ErrorMessage name="email" component={ErrorText} />
+                </ErrorMessageContainer>
+              </FieldContainer>
+            </FormRow>
+
+            <FormRowName>
+              <label htmlFor="name">Ваше имя</label>
+              <StyledField type="text" name="name" />
+            </FormRowName>
+
+            <StyledButton
+              type="submit"
+              disabled={!isValid || !dirty || isSubmitting}
+            >
+              Создать бронирование
+            </StyledButton>
+          </StyledForm>
+        );
+      }}
     </Formik>
   );
 };
