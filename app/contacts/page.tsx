0501ca4dfabbf0ad12bd527514ff92daca8e3535
iavianm <iavianm@mail.ru>
2024-01-09 19:48:27 +0200

    Meta

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,5 +1,12 @@
 import styles from "./page.module.css";
 import About from "@/components/About/About";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Контакты | Отдых на природе у воды",
+  description:
+    "Стильные дома для выходных в природе, Приватные виллы для отдыха у моря, Современные дома для семейного отдыха рядом с Петербургом, Вилла для отпуска в экологически чистом районе, Отдых в экологически чистых местах, Путешествие с семьей на Финский залив",
+};
 
 const Contacts = () => {
   return (
--- app/houses/page.tsx
+++ app/houses/page.tsx
@@ -5,6 +5,13 @@ import House from "@/components/House/House";
 import { riviera, grandis } from "../../content/housesContent.json";
 import housesSlider from "../../content/housesSliderContent.json";
 import Terrace from "@/components/Terrace/Terrace";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Номера | Уютные коттеджи для отдыха",
+  description:
+    "Премиум отдых в России ,Уютные коттеджи для отдыха, Коттедж в скандинавском стиле с панорамными окнами,Современные виллы для аренды, Люкс коттеджи для отпуска рядом с Питером, Отдых в сосновом лесу у воды, Роскошные виллы для отпуска ",
+};
 
 const Houses = () => {
   return (
